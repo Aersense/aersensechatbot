a8c11393bcd41be28699b69766f5b2e14073e9bb
Saurav <sauravkumar00108@gmail.com>
2024-08-27 19:28:00 +0530

    chore: Update transition duration for tooltip animation

--- src/views/Homescreen/Homescreen.js
+++ src/views/Homescreen/Homescreen.js
@@ -14,7 +14,7 @@ function Homescreen() {
         tooltip.classList.add('show');
         const timer = setTimeout(() => {
             tooltip.classList.remove('show');
-        }, 3000);
+        }, 5000);
         return () => clearTimeout(timer);
     }, []);
 
